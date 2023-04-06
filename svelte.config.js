d4131c38fee84785ecd72391ff660a2f33dff674
afn <hey@afn.lol>
2023-04-06 01:22:15 -0400

    uiiiu

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [vitePreprocess()],
 
 	kit: {
-		// adapter-static has vercel detection, but that does not let you set a custom 404 page easily.
-		// Instead, we have to use a wrapper that generates a vercel config if on vercel...
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
 			fallback: null,
 			precompress: false,
 			strict: true
-		})
+		}),
+		env: {
+			publicPrefix: 'RV'
+		}
 	}
 };
 
